aabfaa989ed5f168dadab30c46344c54c83df365
miraz66 <mirajulislam0509@gmail.com>
2023-09-01 01:55:05 +0600

    studioAbout

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -34,6 +34,8 @@ function About() {
       <div className="z-10">sd sdf sdfsdfsdf</div>
       <div className="z-10">sd sdf sdfsdfsdf</div>
       <div className="z-10">sd sdf sdfsdfsdf</div>
+      <div className="z-10">sd sdf sdfsdfsdf</div>
+      <div className="z-10">sd sdf sdfsdfsdf</div>
     </div>
   );
 }
