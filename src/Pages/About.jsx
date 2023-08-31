253de6250e590cce481104a2a5ead8f5fe4f2623
miraz66 <mirajulislam0509@gmail.com>
2023-09-01 01:57:02 +0600

    studio Home

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -28,10 +28,6 @@ function About() {
       <div className="z-10">sd sdf sdfsdfsdf</div>
       <div className="z-10">sd sdf sdfsdfsdf</div>
       <div className="z-10">sd sdf sdfsdfsdf</div>
-      <div className="z-10">sd sdf sdfsdfsdf</div>
-      <div className="z-10">sd sdf sdfsdfsdf</div>
-      <div className="z-10">sd sdf sdfsdfsdf</div>
-      <div className="z-10">sd sdf sdfsdfsdf</div>
     </div>
   );
 }
