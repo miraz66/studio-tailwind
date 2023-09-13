d5013424e3e587aa86a6202e817e5c68c8fc3fcc
miraz66 <mirajulislam0509@gmail.com>
2023-09-13 22:47:48 +0600

    EmployeeImages

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -315,7 +315,7 @@ export default function Employee() {
                     </div>
                   </li>
 
-                  {/* <li>
+                  <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -339,7 +339,7 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li> */}
+                  </li>
 
                   {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
