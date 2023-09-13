bc4814816e8a178b13d376e5989666b0c7b6265c
miraz66 <mirajulislam0509@gmail.com>
2023-09-13 22:45:42 +0600

    About

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,3 +1,16 @@
+import image1 from "../assets/EmployeeImages/leslie-alexander1.jpg";
+import image2 from "../assets/EmployeeImages/michael-foster2.jpg";
+import image3 from "../assets/EmployeeImages/dries-vincent3.jpg";
+import image4 from "../assets/EmployeeImages/chelsea-hagon4.jpg";
+import image5 from "../assets/EmployeeImages/emma-dorsey5.jpg";
+import image6 from "../assets/EmployeeImages/leonard-krasner6.jpg";
+import image7 from "../assets/EmployeeImages/blake-reid7.jpg";
+import image8 from "../assets/EmployeeImages/kathryn-murphy8.jpg";
+import image9 from "../assets/EmployeeImages/whitney-francis9.jpg";
+import image10 from "../assets/EmployeeImages/jeffrey-webb10.jpg";
+import image11 from "../assets/EmployeeImages/benjamin-russel11.jpg";
+import image12 from "../assets/EmployeeImages/angela-fisher12.jpg";
+
 export default function Employee() {
   return (
     <>
@@ -20,7 +33,7 @@ export default function Employee() {
                   role="list"
                   className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8"
                 >
-                  <li>
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -31,8 +44,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fleslie-alexander.cf5840d1.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fleslie-alexander.cf5840d1.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fleslie-alexander.cf5840d1.jpg&amp;w=3840&amp;q=75"
+                          src={image1}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -45,8 +57,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -57,8 +70,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmichael-foster.00319162.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmichael-foster.00319162.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmichael-foster.00319162.jpg&amp;w=3840&amp;q=75"
+                          src={image2}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -71,8 +83,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -83,8 +96,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdries-vincent.5cac3ed5.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdries-vincent.5cac3ed5.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdries-vincent.5cac3ed5.jpg&amp;w=3840&amp;q=75"
+                          src={image3}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -97,11 +109,12 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
+                  </li> */}
                 </ul>
               </div>
             </div>
           </div>
+
           <div>
             <div
               className="relative before:absolute after:absolute before:bg-neutral-950 after:bg-neutral-950/10 before:left-0 before:top-0 before:h-px before:w-6 after:left-8 after:right-0 after:top-0 after:h-px"
@@ -118,7 +131,7 @@ export default function Employee() {
                   role="list"
                   className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8"
                 >
-                  <li>
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -129,8 +142,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fchelsea-hagon.073aa8f2.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fchelsea-hagon.073aa8f2.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fchelsea-hagon.073aa8f2.jpg&amp;w=3840&amp;q=75"
+                          src={image4}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -143,8 +155,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -155,8 +168,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Femma-dorsey.4fab86fa.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Femma-dorsey.4fab86fa.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Femma-dorsey.4fab86fa.jpg&amp;w=3840&amp;q=75"
+                          src={image5}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -169,8 +181,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -181,8 +194,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fleonard-krasner.5a4a4614.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fleonard-krasner.5a4a4614.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fleonard-krasner.5a4a4614.jpg&amp;w=3840&amp;q=75"
+                          src={image6}
                           style={{ color: "transparent" }}
                         />
 
@@ -196,8 +208,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -208,8 +221,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fblake-reid.bcc5ac4f.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fblake-reid.bcc5ac4f.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fblake-reid.bcc5ac4f.jpg&amp;w=3840&amp;q=75"
+                          src={image7}
                           style={{ color: "transparent" }}
                         />
 
@@ -223,8 +235,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -235,8 +248,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkathryn-murphy.550962fd.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkathryn-murphy.550962fd.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fkathryn-murphy.550962fd.jpg&amp;w=3840&amp;q=75"
+                          src={image8}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -249,8 +261,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -261,8 +274,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fwhitney-francis.ced7480b.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fwhitney-francis.ced7480b.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fwhitney-francis.ced7480b.jpg&amp;w=3840&amp;q=75"
+                          src={image9}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -275,8 +287,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -287,8 +300,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjeffrey-webb.a90fe9d1.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjeffrey-webb.a90fe9d1.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fjeffrey-webb.a90fe9d1.jpg&amp;w=3840&amp;q=75"
+                          src={image10}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -301,8 +313,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -313,8 +326,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbenjamin-russel.da51eb4b.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbenjamin-russel.da51eb4b.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbenjamin-russel.da51eb4b.jpg&amp;w=3840&amp;q=75"
+                          src={image11}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -327,8 +339,9 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
-                  <li>
+                  </li> */}
+
+                  {/* <li>
                     <div style={{ opacity: 1, transform: "none" }}>
                       <div className="group relative overflow-hidden rounded-3xl bg-neutral-100">
                         <img
@@ -339,8 +352,7 @@ export default function Employee() {
                           decoding="async"
                           data-nimg="1"
                           className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
-                          //   srcset="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fangela-fisher.f2122cd4.jpg&amp;w=1920&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fangela-fisher.f2122cd4.jpg&amp;w=3840&amp;q=75 2x"
-                          src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fangela-fisher.f2122cd4.jpg&amp;w=3840&amp;q=75"
+                          src={image12}
                           style={{ color: "transparent" }}
                         />
                         <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black to-black/0 to-40% p-6">
@@ -353,7 +365,7 @@ export default function Employee() {
                         </div>
                       </div>
                     </div>
-                  </li>
+                  </li> */}
                 </ul>
               </div>
             </div>
