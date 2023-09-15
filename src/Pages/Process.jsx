ccd05fb60f496893ecb2cee7ac915c961f5172ab
miraz66 <mirajulislam0509@gmail.com>
2023-09-15 17:45:26 +0600

    Process

--- src/Pages/Process.jsx
+++ src/Pages/Process.jsx
@@ -168,6 +168,8 @@ export default function Process() {
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
       </MainContainer>
     </div>
   );
