99ffe64c554b075590952d27ae7df99e6c7282f7
miraz66 <mirajulislam0509@gmail.com>
2023-09-15 17:48:40 +0600

    ProcessDiscoverImages

--- src/Pages/Process.jsx
+++ src/Pages/Process.jsx
@@ -201,6 +201,8 @@ export default function Process() {
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
       </MainContainer>
     </div>
   );
