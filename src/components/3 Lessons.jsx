da3b3b749b47b716e6b66775a5c92f89d15a5366
miraz66 <mirajulislam0509@gmail.com>
2023-09-16 22:30:06 +0600

    FutureOfWebDevelopment

--- src/components/3 Lessons.jsx
+++ src/components/3 Lessons.jsx
@@ -18,6 +18,8 @@ export default function Lessons() {
       <div className=""></div>
       <div className=""></div>
       <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
     </>
   );
 }
