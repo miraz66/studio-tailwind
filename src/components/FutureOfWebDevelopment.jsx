99c83a21be3550fbac9a0fa371d1e4fd1387be90
miraz66 <mirajulislam0509@gmail.com>
2023-09-16 19:53:47 +0600

    Blog

--- src/components/FutureOfWebDevelopment.jsx
+++ src/components/FutureOfWebDevelopment.jsx
@@ -3,33 +3,31 @@ import HomeDisplay from "./HomeDisplay";
 
 export default function FutureOfWebDevelopment() {
   return (
-    <div>
-      <div className="">
-        <MainContainer>
-          <HomeDisplay css="h-[42rem] xl:h-[48rem] 2xl:h-[30rem] relative">
-            <div className="absolute top-64 w-full">
-              <div className="max-w-7xl mx-auto px-5">
-                <div className="text-center">
-                  <h1>
-                    <span className="block font-display text-sm font-medium text-neutral-950">
-                      April 6, 2023
-                    </span>
-                    <span className="sr-only"> - </span>
-                    <span className="mt-6 block font-display font-semibold tracking-tighter text-neutral-800 [text-wrap:balance] sm:text-5xl">
-                      The Future of Web Development: Our Predictions for 2023
-                    </span>
-                  </h1>
-                  <div className="mt-6 text-base text-neutral-600">
-                    <p className="pb-10 text-neutral-600 font-medium tracking-tighter leading-8">
-                      by Chelsea Hagon, Senior Developer
-                    </p>
-                  </div>
+    <>
+      <MainContainer>
+        <HomeDisplay css="h-[42rem] xl:h-[48rem] 2xl:h-[30rem] relative">
+          <div className="absolute top-64 w-full">
+            <div className="max-w-7xl mx-auto px-5">
+              <div className="text-center">
+                <h1>
+                  <span className="block font-display text-sm font-medium text-neutral-950">
+                    April 6, 2023
+                  </span>
+                  <span className="sr-only"> - </span>
+                  <span className="mt-6 block font-display font-semibold tracking-tighter text-neutral-800 [text-wrap:balance] sm:text-5xl">
+                    The Future of Web Development: Our Predictions for 2023
+                  </span>
+                </h1>
+                <div className="mt-6 text-base text-neutral-600">
+                  <p className="pb-10 text-neutral-600 font-medium tracking-tighter leading-8">
+                    by Chelsea Hagon, Senior Developer
+                  </p>
                 </div>
               </div>
             </div>
-          </HomeDisplay>
-        </MainContainer>
-      </div>
-    </div>
+          </div>
+        </HomeDisplay>
+      </MainContainer>
+    </>
   );
 }
