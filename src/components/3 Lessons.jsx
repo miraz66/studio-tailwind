fd0baaf63372d6ae338a194241267a23fb414810
miraz66 <mirajulislam0509@gmail.com>
2023-09-16 22:31:04 +0600

    FutureOfWebDevelopment Footer

--- src/components/3 Lessons.jsx
+++ src/components/3 Lessons.jsx
@@ -42,6 +42,8 @@ export default function Lessons() {
       <div className=""></div>
       <div className=""></div>
       <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
     </>
   );
 }
