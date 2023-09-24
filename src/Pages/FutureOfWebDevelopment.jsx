56c303c7b623369ca075dc2167d6c61e2c4ecd51
miraz66 <mirajulislam0509@gmail.com>
2023-09-24 11:43:29 +0600

    Res

--- src/Pages/FutureOfWebDevelopment.jsx
+++ src/Pages/FutureOfWebDevelopment.jsx
@@ -8,7 +8,7 @@ export default function FutureOfWebDevelopment() {
   return (
     <>
       <MainContainer>
-        <HomeDisplay css="h-[35rem] md:h-[42rem] xl:h-[48rem] 2xl:h-[30rem] relative">
+        <HomeDisplay css="h-[33rem] md:h-[42rem] xl:h-[48rem] 2xl:h-[30rem] relative">
           <div className="absolute top-52 md:top-64 w-full">
             <div className="max-w-7xl mx-auto px-5">
               <div className="text-center">
