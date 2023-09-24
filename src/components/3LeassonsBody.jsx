abc902b78556530c701a977f64cae4469c1a78bd
miraz66 <mirajulislam0509@gmail.com>
2023-09-24 23:49:25 +0600

    Working Process Res

--- src/components/3LeassonsBody.jsx
+++ src/components/3LeassonsBody.jsx
@@ -119,7 +119,7 @@ export default function LeassonsBody() {
         </div>
 
         {/* ----More Article---- */}
-        <div className="h-[30rem] mt-20 lg:mt-40 relative overflow-hidden rounded-t-[3rem] bg-gradient-to-b from-neutral-50">
+        <div className="h-[57rem] lg:h-[35rem] xl:h-[30rem] mt-20 lg:mt-40 relative overflow-hidden rounded-t-[3rem] bg-gradient-to-b from-neutral-50">
           <BodySvg />
           <div className="top-0 left-0 w-full absolute">
             <div className="max-w-7xl mx-auto px-5 pt-28 lg:py-40">
